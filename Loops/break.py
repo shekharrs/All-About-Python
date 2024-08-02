71920b24c6deff8c19c113685703a06265a794e4
shekhar <shekhart442@gmail.com>
2024-08-02 21:20:33 +0530

    implement break statement concept

--- Loops/break.py
+++ Loops/break.py
@@ -1,8 +1,31 @@
 # Break in Python
 
-n = int(input("Enter a number: "))
+# Using for loop
+# n = int(input("Enter a number: "))
 
-for x in range(2,n+1) :
-    if n % x == 0 :
-        print(x)
-        
+# for x in range(2,n+1) :
+#     if n % x == 0 :
+#         print(x)
+#         break
+
+###############################################
+
+# Using while loop
+# n = int(input("Enter a number: "))
+
+# x = 2
+# while x <=n :
+#     if n % x == 0 :
+#         print(x)
+#         break
+#     x = x + 1
+
+###############################################
+
+i = 1
+while i <= 5 :
+    if i == 3 :
+        break
+    print(i)
+    i = i + 1
+print(i)
