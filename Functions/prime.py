7125e24e12a4ec3514502c7ea9847fd8ba91eeae
shekhar <shekhart442@gmail.com>
2024-08-09 13:09:29 +0530

    updated

---
+++ Functions/prime.py
@@ -0,0 +1,24 @@
+# Prime Factorization
+
+# def isPrime(x):
+#     for i in range(2, x):
+
+#         if x % i == 0:
+#             return False
+#     return True
+    
+# def printPFactor(n):
+#     for i in range(2, n+1):
+#         if isPrime(i):  # checking prime for i
+#             x = i
+#             while n % x == 0:
+#                 print(i)
+#                 x = x * i # next power of i
+
+# n = int(input("Enter n: "))
+# printPFactor(n)  
+
+
+y=8
+z= lambda x:x*y
+print(z(6))
